ee84b0dd3486fa630a2e8ad840404f6921abd822
suphachaiinet <109581845+suphachaiinet@users.noreply.github.com>
2025-01-09 13:54:27 +0700

    email_okay

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ app.secret_key = 'your_secret_key'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_email_app_password'
-app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
+app.config['MAIL_USERNAME'] = 's6506022410031@email.kmutnb.ac.th'
+app.config['MAIL_PASSWORD'] = 'y053645033'
+app.config['MAIL_DEFAULT_SENDER'] = 'ys6506022410031@email.kmutnb.ac.th'
 mail = Mail(app)
 
 # ใช้ Flask-Session เพื่อจัดการเซสชัน
